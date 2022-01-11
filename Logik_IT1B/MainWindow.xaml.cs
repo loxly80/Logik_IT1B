f99f10069f97b109de192d7e5638f3be7d922a12
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-01-11 11:36:58 +0100

    konec hodiny

--- Logik_IT1B/MainWindow.xaml.cs
+++ Logik_IT1B/MainWindow.xaml.cs
@@ -23,11 +23,53 @@ namespace Logik_IT1B
         public MainWindow()
         {
             InitializeComponent();
+            NastavVariantu0(false);
         }
 
         private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            MessageBox.Show("Hops");
+            Ellipse ellipse = (Ellipse)sender;
+
+            if (ellipse.Fill.ToString() == "#FFD9D9D9")
+            {
+                ellipse.Fill = Brushes.Red;
+            }
+            else if (ellipse.Fill == Brushes.Red)
+            {
+                ellipse.Fill = Brushes.Green;
+            }
+            else if (ellipse.Fill == Brushes.Green)
+            {
+                ellipse.Fill = Brushes.Blue;
+            }
+            else if (ellipse.Fill == Brushes.Blue)
+            {
+                ellipse.Fill = Brushes.Yellow;
+            }
+            else if (ellipse.Fill == Brushes.Yellow)
+            {
+                ellipse.Fill = Brushes.Orange;
+            }
+            else if (ellipse.Fill == Brushes.Orange)
+            {
+                ellipse.Fill = Brushes.Purple;
+            }
+            else
+            {
+                ellipse.Fill = Brushes.Red;
+            }
         }
-    }
+
+        private void NastavVariantu0(bool aktivni)
+        {
+            if (aktivni)
+            {
+                Ellipse_0_0.MouseDown += Ellipse_MouseDown;
+            }
+            else
+            {
+                Ellipse_0_0.MouseDown -= Ellipse_MouseDown;
+            }
+        }
+    }    
 }
